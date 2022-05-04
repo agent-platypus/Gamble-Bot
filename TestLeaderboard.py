e05cc62dc768c21ef3c2ebfcb3b5085384f3764a
Daniel Kim <daniel.kim0135@gmail.com>
2022-05-03 20:17:53 -0700

    added test leaderboard and added addPlayer func to leaderboard.py

---
+++ TestLeaderboard.py
@@ -0,0 +1,39 @@
+from re import X
+import unittest
+from leaderboard import Leaderboard
+
+from player import INIT_MONEY, Player
+
+class TestLeaderboard(unittest.TestCase):
+
+    def setUp(self) -> None:
+        playerlist = [Player('ken'), Player('ray'), Player('mau'), Player('david'), Player('brandon')]
+        playerlist[0].subtractMoney(10)
+        playerlist[1].subtractMoney(25)
+        playerlist[2].subtractMoney(15)
+        playerlist[3].subtractMoney(5)
+        playerlist[4].subtractMoney(30)
+        self.leaderboard = Leaderboard(playerlist)
+
+    def testSortMoney(self):
+        #sorting expected: david, ken, mau, ray, brandon
+
+        # testing number 1 standing
+        self.assertEqual(self.leaderboard.players[0].money, 200)
+    
+    def testSortNames(self):
+        # testing proper standing with corresponding names
+        self.assertEqual(self.leaderboard.players[0].name, 'david')
+        self.assertEqual(self.leaderboard.players[1].name, 'ken')
+        self.assertEqual(self.leaderboard.players[2].name, 'mau')
+        self.assertEqual(self.leaderboard.players[3].name, 'ray')
+        self.assertEqual(self.leaderboard.players[4].name, 'brandon')
+
+    def testAddplayer(self):
+        self.leaderboard.addPlayer(Player('nancy'))
+        self.assertEqual(self.leaderboard.players[0].name, 'nancy')
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
--- leaderboard.py
+++ leaderboard.py
@@ -2,11 +2,17 @@ from player import Player
 
 class Leaderboard:
     def __init__(self, players: list[Player]):
-        self.players = []
-        for player in players:
-            self.players.append(player)
+        self.players = players
         self.sortLeaderboard()
+    
 
     # sort leaderboard by money
     def sortLeaderboard(self):
         self.players.sort(key=lambda x: x.money, reverse=True)
+
+    def addPlayer(self, pushinp: Player):
+        # O(n)
+        self.players.append(pushinp)
+    
+
+    # def printLeaderBoard(self):
